60c72132cb6ccec1e5406db6e8ea9cce8708aaa6
Vladimir Larin <236788621+vv-larin@users.noreply.github.com>
2026-08-21 01:46:38 +0300

    Implemented basic movement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+const MAGE_SPEED: f32 = 100.0;
+
 #[derive(Component)]
 struct Mage;
 
@@ -7,6 +9,7 @@ fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
     app.add_systems(Startup, setup);
+    app.add_systems(Update, move_mage);
     app.run();
 }
 
@@ -17,4 +20,35 @@ fn setup(mut commands: Commands) {
         Sprite::from_color(Color::WHITE, Vec2::new(20.0, 20.0)),
         Transform::from_xyz(100.0, 0.0, 0.0),
     ));
+}
+
+fn move_mage(
+    key: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<Mage>>,
+) {
+    let mut transform = match query.single_mut() {
+        Ok(transform) => transform,
+        Err(error) => {
+            panic!("There should be exactly one mage: {error}");
+        }
+    };
+
+    let mut move_dir = Vec2::ZERO;
+
+    if key.pressed(KeyCode::KeyW) {
+        move_dir.y += 1.0;
+    }
+    if key.pressed(KeyCode::KeyS) {
+        move_dir.y -= 1.0;
+    }
+    if key.pressed(KeyCode::KeyD) {
+        move_dir.x += 1.0;
+    }
+    if key.pressed(KeyCode::KeyA) {
+        move_dir.x -= 1.0;
+    }
+
+    transform.translation +=
+        MAGE_SPEED * time.delta_secs() * move_dir.normalize_or_zero().extend(0.0);
 }
